593cfca261d7d2d8e6d6234f55848b1f4e1f6ac6
Allison Kwan <akwan2020@gmail.com>
2021-03-09 11:51:01 -0500

    Implemented ImagePicker functionality

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react';
+import { StyleSheet, Text, View, Image, Button} from 'react-native';
+import * as ImagePicker from 'react-native-image-picker';
+
+export default class App extends React.Component {
+    state = {
+        photo: null,
+    }
+    handleChoosePhoto = () => {
+        const options = {
+            noData: true,
+        };
+        ImagePicker.launchImageLibrary(options, (response) =>{
+            // console.log("response",response);
+            if (response.uri) {
+                this.setState({ photo: response });
+            }
+        });
+    };
+
+    render() {
+      const { photo } = this.state;
+      return (
+          <View style={ styles.container }>
+              <Text style={styles.boldText}>Crop Disease Detector</Text>
+              <View style = {styles.white}>
+              </View>
+              <View style = {styles.gray}>
+              </View>
+              {photo && (
+                  <Image
+                      source={{ uri: photo.uri }}
+                      style={{width: 180, height: 200 }}
+                  />
+              )}
+                <Button
+                  title="Upload Image Here"
+                  color="#000000"
+                  onPress={this.handleChoosePhoto}
+                />
+          </View>
+      );
+  }
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ // <View style={styles.container}>
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#00ffff', // teal background color
     justifyContent: 'center',
+      alignItems: "center"
   },
+    boldText: {
+      fontWeight: 'bold',
+        color: 'white',
+        fontSize: 25,
+        top: 20,
+        left: 20,
+        position: 'absolute'
+    },
+    white: {
+      backgroundColor: '#ffffff',
+        top: 70,
+        left: 20,
+        width: 280,
+        height: 600,
+        position: 'absolute'
+    },
+    gray: {
+        backgroundColor: '#BEBEBE',
+        top: 180,
+        left: 65,
+        width: 180,
+        height: 200,
+        position: 'absolute'
+    }
 });
